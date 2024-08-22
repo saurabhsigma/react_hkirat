78161aef2ffb69578cb45719092ffda706acced0
saurabhsigma <saurabh962003@gmail.com>
2024-08-23 00:47:13 +0530

    useEffect done

--- 6.2/vite-project/README.md
+++ 6.2/vite-project/README.md
@@ -7,8 +7,20 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
+# common hooks in react
 
 <!-- 
- 6.2  | useEffect, useMemo, useCallback
+ 6.2  | useEffect, useMemo, useCallback, custom hooks and prop drilling 
 
  -->
+
+ <!-- any thing which is not related to rendering 
+ putting things on the dom and taking things out of the dom, is called side effects. few examples could be fetching data from the server/backend.  -->
+
+ <!-- some common hooks are:
+ 1. useState
+ 2. useEffect
+ 3. useCallback
+ 4. useRef
+ 5. useMemo
+ 6. useContext  -->
--- 6.2/vite-project/src/App.jsx
+++ 6.2/vite-project/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   // useState
+//   // const [count, setCount] = useState(0);
+//   // return <div>
+//   //   <button onClick={function () {
+//   //     setCount(count + 1);
+//   //   }}> Click Me {count}</button>
+//   // </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// }
+
+// export default App;
+
+// import React from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
+
+// function App(){
+//   const [todos, setTodos] = useState([])
+
+  // use can use the axios syntax in the place of fetch syntax as an option
+  // just import axios and write the following code in place of fetcg
+  // useEffect(()=>{
+  //   axios.get("https://sum-server.100xdevs.com/todos")
+  //     .then(function(response) {
+  //       setTodos(response.data.todos)
+  //     })
+  // },[])
+
+  // useEffect(()=>{
+  //   fetch("https://sum-server.100xdevs.com/todos")
+  //   .then(async function(res){
+  //     const json = await res.json();
+  //     setTodos(json.todos);
+  //   })
+  // },[])
+
+  // the second argument in the useffect (,[]) gives conditions as to when to call the fetch function
+  // useEffect is used as a pit stop, which will be not used in every render
+  // but when we want to use it, we can give it a condition
+
+//   return <div>
+//     {todos.map(todo => <Todo key={todo.title} title={todo.title} description = {todo.description} />)}
+//   </div>
+// }
+
+
+// problem statement
+// write a component that takes a todo id as an input and fetches 
+// the data for that todo from the given data endpoint
+// and then renders it
+// how would the dependency array change?
+
+
+import { useState } from "react";
+import { useEffectd  } from "react";
+
+function App(){
+  return <div>
+    <Todo id = {1} />
+  </div>
 }
 
-export default App
+function Todo({id}){
+  const [todos, setTodos] = useState({});
+
+  // your effect here
+
+  return <div>
+    <h1>
+      {todos.title}
+    </h1>
+    <h4>
+      {todos.description}
+    </h4>
+  </div>
+}
+
+export default App;
