295a32f2a155c267404ae532b98d8833a675db70
saurabhsigma <saurabh962003@gmail.com>
2024-09-11 10:04:42 +0530

    11 sept

--- 7.1/my-app/src/components/Dashboard.jsx
+++ 7.1/my-app/src/components/Dashboard.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 export function Dashboard(){
+    const navigate = useNavigate();
+
+    function handleClick(){
+        navigate('/')
+    };
+
     return (
         <div>
-            Dashboard Page
+            Dashboard
+            <button onClick={handleClick}>Click to navigate</button>
         </div>
     )
 }
