a9a264ed5fd47207dbae455aafe4c26010de3d3c
saurabhsigma <saurabh962003@gmail.com>
2024-09-12 19:08:39 +0530

    12 sept

--- 7.1/my-app/src/App.jsx
+++ 7.1/my-app/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import { Dashboard } from './components/Dashboard'
 import { Landing } from './components/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  
+
 
   return (
     <div>
-      <div>
-        <button onClick={()=>{
-          window.location.href= "/landing"
-        }}>Landing</button>
-        <button onClick={()=>{
-          window.location.href= "/dashboard"
-        }}>Dashboard</button>
-      </div>
+
     <BrowserRouter>
+      <Appbar />
       <Routes>
          < Route path='/dashboard' element={<Dashboard />} />
          < Route path='/landing' element={<Landing />} />
@@ -28,4 +24,21 @@ function App() {
   )
 }
 
+function Appbar(){
+  const navigate = useNavigate();
+  return(
+  <div>
+        <button onClick={()=>{
+          navigate("/landing");
+        }}>Landing</button>
+        <button onClick={()=>{
+          navigate("/dashboard")
+        }}>Dashboard</button>
+      </div>
+
+  )
+}
+
 export default App
+
+
--- 7.1/my-app/src/components/Dashboard.jsx
+++ 7.1/my-app/src/components/Dashboard.jsx
@@ -1,16 +1,37 @@
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from "react-router-dom"
+
+// import { useNavigate } from "react-router-dom";
 
 export function Dashboard(){
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    function handleClick(){
-        navigate('/')
-    };
+    // function handleClick(){
+    //     navigate('/')
+    // };
 
     return (
         <div>
             Dashboard
-            <button onClick={handleClick}>Click to navigate</button>
+            {/* <button onClick={handleClick}>Click to navigate</button> */}
         </div>
     )
 }
+
+
+
+
+// export default function Dashboard() {
+//     const navigate = useNavigate();
+
+
+//     function handleClick() {
+//         navigate('/')
+//     };
+
+//     return <div>
+
+//         Dashboard
+//         <button onClick={handleClick}>Click here to navigate!</button>
+//     </div>
+// }
+
